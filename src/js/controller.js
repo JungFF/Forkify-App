3c0733a5f9c6b5d7fe901403192dce27dc613991
JungFF <wangjung80@gmail.com>
2022-03-14 12:51:53 -0400

    Loading a Recipe from API

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,7 +7,26 @@ const timeout = function (s) {
     }, s * 1000);
   });
 };
+const showRecipe = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604691c37cdc054bd0d4');
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    }
+    console.log(recipe);
+  }
+  catch (err) {
+    alert(err);
+  }
+}
+showRecipe();
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////

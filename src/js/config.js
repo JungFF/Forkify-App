7a7179e223153065adae0e45df908e2bcfb8b0be
JungFF <wangjung80@gmail.com>
2022-03-17 12:54:46 -0400

    change the API-KEY for testing

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 12;
-export const KEY = '464f819b-a4fe-4e42-bd82-98edb02f01b8';
+export const KEY = '4743b9d2-1ced-4f68-9608-8728e239bf68';
 export const MODAL_CLOSE_SEC = 0.5;
 // '74cad1e7-bea6-4309-b2f7-961c36658d73'
